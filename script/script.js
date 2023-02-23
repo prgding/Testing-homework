6a15723d5638dd93c45f7a9d7a678a3f7b01c752
Ding Shuai <1203823603@qq.com>
2023-02-23 17:31:06 +0800

    优化regex、添加按钮

--- script/script.js
+++ script/script.js
@@ -1,29 +1,22 @@
 function CheckItem(form) {
-	const phoneRegex = /^\d{4}-?\d{8}$/;
-	const phoneRegex2 = /\d{8}/
+	const phoneRegex = /^(\d{4}-)?\d{8}$/;
 	const phoneNumber = form.tele.value.trim();
-
-	// 没有 -
-	if (phoneNumber.indexOf('-') == -1) {
-		// 不是数字
-		if (!phoneRegex2.test(phoneNumber)) {
-			alert("电话号码格式不正确，请输入正确的电话号码。");
-			window.location = "index.html"
-			
-		} else if (phoneNumber.length != 8) {
-			alert("电话号码格式不正确，请输入正确的电话号码。");
-			window.location = "index.html"
-		} else {
-			window.location = "success.html"
-		}
+	if (!phoneRegex.test(phoneNumber)) {
+		alert("电话号码格式不正确");
+		window.location = "index.html"
 	} else {
-		// 有 -
-		if (!phoneRegex.test(phoneNumber)) {
-			alert("电话号码格式不正确，请输入正确的电话号码。");
-			return false;
-		} else {
-			window.location = "success.html"
-		}
+		window.location = "success.html"
 	}
 	return true;
+}
+
+function showRules() {
+	alert("电话号码规则：\n" +
+		"1.电话号码应为数字\n" +
+		"2.如带区号则在区号与号码中间插入-号\n" +
+		"3.电话号码为8位数字,区号为4位数字");
+}
+
+function Clear() {
+	document.getElementById("tele").value = ''
 }
